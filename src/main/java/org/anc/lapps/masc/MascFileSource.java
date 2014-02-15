d69cbb00ce5b409bf33887d0084e8f219257d870
Keith Suderman <suderman@anc.org>
2014-02-15 16:30:08 -0500

    Bumped version. Renamed files.

--- src/main/java/org/anc/lapps/masc/MascFileSource.java
+++ src/main/java/org/anc/lapps/masc/MascFileSource.java
@@ -10,10 +10,16 @@ import java.io.IOException;
 /**
  * A @{link org.lappsgrid.api.DataSource DataSource} for accessing all files
  * in MASC.
+ * <p>
+ * The @{link org.and.lapps.masc.index.MascFullIndex MascFullIndex} uses the
+ * document id with the annotation type appended as the key for the index.
+ * For example
+ *    MASC3-0202-txt
+ *    MASC3-0202-hdr
+ *    MASC3-0202-nc
  *
  * @author Keith Suderman
  */
-//@WebService
 public class MascFileSource extends AbstractDataSource
 {
    public MascFileSource() throws IOException
