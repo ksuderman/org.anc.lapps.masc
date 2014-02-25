5028d6290c11161fe2b3349cae45278a5f67b9d3
Keith Suderman <suderman@anc.org>
2014-02-25 10:37:09 -0500

    Finished MascJsonSource

--- src/main/java/org/anc/lapps/masc/MascJsonSource.java
+++ src/main/java/org/anc/lapps/masc/MascJsonSource.java
@@ -1,35 +1,35 @@
 package org.anc.lapps.masc;
 
 import org.anc.lapps.core.AbstractDataSource;
-//import org.anc.lapps.masc.index.MascFullIndex;
 import org.anc.lapps.masc.index.MascJsonIndex;
 import org.lappsgrid.api.Data;
 import org.lappsgrid.discriminator.Types;
 
 import java.io.IOException;
 
+
 /**
- *
  * @author Jesse Stuart
  */
 public class MascJsonSource extends AbstractDataSource
 {
-    public MascJsonSource() throws IOException
-    {
-        super(new MascJsonIndex());
-    }
 
-    @Override
-    /**
-     * The text data source either returns an error or the actual text.
-     */
-    protected Data get(String key)
-    {
-        Data result = super.get(key);
-        if (result.getDiscriminator() != Types.ERROR)
-        {
-            result.setDiscriminator(Types.TEXT);
-        }
-        return result;
-    }
+   public MascJsonSource() throws IOException
+   {
+      super(new MascJsonIndex());
+   }
+
+   @Override
+   /**
+    * The text data source either returns an error or the actual text.
+    */
+   protected Data get(String key)
+   {
+      Data result = super.get(key);
+      if (result.getDiscriminator() != Types.ERROR)
+      {
+         result.setDiscriminator(Types.JSON);
+      }
+      return result;
+   }
 }
