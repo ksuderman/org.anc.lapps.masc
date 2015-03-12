5a423ce2843ccb2a9f78bcfd486906c069497f9b
Keith Suderman <suderman@anc.org>
2015-03-12 16:01:35 -0400

    Trying to get annotations working again.

--- src/main/java/org/anc/lapps/masc/MascJsonSource.java
+++ src/main/java/org/anc/lapps/masc/MascJsonSource.java
@@ -2,7 +2,7 @@ package org.anc.lapps.masc;
 
 import org.anc.lapps.masc.index.MascJsonIndex;
 import org.lappsgrid.discriminator.Discriminators;
-//import org.lappsgrid.experimental.annotations.DataSourceMetadata;
+import org.lappsgrid.experimental.annotations.DataSourceMetadata;
 
 import java.io.IOException;
 
@@ -11,15 +11,15 @@ import java.io.IOException;
  * @author Jesse Stuart
  * @author Keith Suderman
  */
-//@DataSourceMetadata(
-//        description = "Annotations from the ANC's Manually Annotated SubCorpus in a JSON container",
-//		  vendor = "http://www.anc.org",
-//		  license = "cc-by",
-//		  allow = "any",
-//		  format = "lapps",
-//		  encoding = "utf-8",
-//		  language = "en-US"
-//)
+@DataSourceMetadata(
+        description = "Annotations from the ANC's Manually Annotated SubCorpus in a JSON container",
+		  vendor = "http://www.anc.org",
+		  license = "cc-by",
+		  allow = "any",
+		  format = "lapps",
+		  encoding = "utf-8",
+		  language = "en-US"
+)
 public class MascJsonSource extends MascAbstractDataSource
 {
 
